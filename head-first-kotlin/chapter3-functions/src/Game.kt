9adb9ceab1921aa20fc4c703ece0137d2d07f85b
nero <phuongsover2@gmail.com>
2025-02-23 00:20:35 +0700

    feat: Add initial game logic with max function
    This commit introduces the basic structure for a game using
    Kotlin, including a `main` function and an array of options.
    It also adds a `max` function to determine the larger of two
    integers, using a more concise expression body syntax.

---
+++ head-first-kotlin/chapter3-functions/src/Game.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+  val options = arrayOf("Rock", "Paper", "Scissors")
+  println(max(1,2))
+
+}
+
+
+//fun max(a: Int, b: Int): Int = if (a > b ) a else b
+// More Shorten Version
+fun max(a: Int, b: Int) = if (a > b) a else b
