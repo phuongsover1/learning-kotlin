ff3cd506fe6879cb40fd104d06618553fa05a532
nero <phuongsover2@gmail.com>
2025-02-23 00:25:11 +0700

    feat(game): add getGameChoice function
    Adds a function to randomly select game options
    
    - Implements `getGameChoice` for random selection
    - Uses `Math.random()` to pick array element
    - Improves game setup with randomized choices

--- head-first-kotlin/chapter3-functions/src/Game.kt
+++ head-first-kotlin/chapter3-functions/src/Game.kt
@@ -1,10 +1,12 @@
 fun main(args: Array<String>) {
   val options = arrayOf("Rock", "Paper", "Scissors")
-  println(max(1,2))
 
 }
 
 
 //fun max(a: Int, b: Int): Int = if (a > b ) a else b
 // More Shorten Version
-fun max(a: Int, b: Int) = if (a > b) a else b
+//fun max(a: Int, b: Int) = if (a > b) a else b
+
+// Code Magnets
+fun getGameChoice(optionsParam: Array<String>) = optionsParam[(Math.random() * optionsParam.size).toInt()]
