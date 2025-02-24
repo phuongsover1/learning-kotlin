16bc00ba76b05c70906405937608d7a6cdbe782a
nero <phuongsover2@gmail.com>
2025-02-24 19:40:32 +0700

    feat: Implement user choice input and validation
    This commit introduces the `getUserChoice` function which:
    
    - Prompts the user to select an option from the provided array.
    - Validates the user input to ensure it is a valid option.
    - Reprompts the user if the input is invalid.
    - Returns the user's choice when it is valid.
    
    This function allows the user to interact with the game.

--- head-first-kotlin/chapter3-functions/src/Game.kt
+++ head-first-kotlin/chapter3-functions/src/Game.kt
@@ -1,6 +1,7 @@
 fun main(args: Array<String>) {
   val options = arrayOf("Rock", "Paper", "Scissors")
-
+  val gameChoice = getGameChoice(options)
+  val userChoice = getUserChoice(options)
 }
 
 
@@ -10,3 +11,24 @@ fun main(args: Array<String>) {
 
 // Code Magnets
 fun getGameChoice(optionsParam: Array<String>) = optionsParam[(Math.random() * optionsParam.size).toInt()]
+
+fun getUserChoice(optionsParam: Array<String>): String {
+  // Read the user input
+  var userChoice = ""
+  var isValidChoice = false
+  while (!isValidChoice) {
+    // Ask the user for their choice
+    print("Please enter one of the following options:")
+    for (item in optionsParam)
+      print(" $item")
+    println(".")
+    val userInput = readLine()
+    // Validate user input
+    if (userInput != null && userInput in optionsParam) {
+      isValidChoice = true
+      userChoice = userInput
+    }
+    if (!isValidChoice) println("You must enter a valid option.")
+  }
+  return userChoice
+}
