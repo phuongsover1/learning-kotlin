f04f24f98ec46bd4260fc8e46511f60ad1e8df21
nero <phuongsover2@gmail.com>
2025-02-24 20:37:27 +0700

    feat(classes): add main function for Dog class
    Adds a main function to demonstrate Dog class usage.
    
    - Creates Dog instances with specified attributes.
    - Modifies the weight of a Dog instance.
    - Iterates through an array of Dogs and calls bark().

--- head-first-kotlin/chapter4-classes/src/Main.kt
+++ head-first-kotlin/chapter4-classes/src/Main.kt
@@ -3,3 +3,13 @@ class Dog (val name: String, var weight: Int, val breed: String) {
         println(if (weight < 20) "Yip!" else "Woof!")
     }
 }
+
+fun main(args: Array<String>) {
+    var myDog = Dog("Fido", 70, "Mixed")
+    println(myDog.name)
+    myDog.weight = 75
+
+    val dogs = arrayOf(Dog("Fido", 70, "Mixed"), Dog("Ripper", 10, "Poodle"))
+    for (dog in dogs)
+        dog.bark()
+}
