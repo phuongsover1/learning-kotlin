853ed70ea42efc8386df439fbb16b9add124fdaa
nero <phuongsover2@gmail.com>
2025-02-24 21:07:03 +0700

    refactor(classes): update Dog class property declaration
    Refactor Dog class to explicitly declare properties in constructor.
    
    - Change from implicit property declaration in primary constructor to explicit property declaration within the class body.
    - Update constructor parameters to temporary variables.
    - Improves code readability and maintainability.

--- head-first-kotlin/chapter4-classes/src/Main.kt
+++ head-first-kotlin/chapter4-classes/src/Main.kt
@@ -1,15 +1,28 @@
-class Dog (val name: String, var weight: Int, val breed: String) {
-    fun bark() {
-        println(if (weight < 20) "Yip!" else "Woof!")
-    }
+//class Dog (val name: String, var weight: Int, val breed: String) {
+//    fun bark() {
+//        println(if (weight < 20) "Yip!" else "Woof!")
+//    }
+//}
+
+class Dog(name_param: String, weight_param: Int, breed_param: String) {
+  val name = name_param
+  var weight = weight_param
+  val breed = breed_param
+
+  fun bark() {
+    println(if (weight < 20) "Yip!" else "Woof!")
+  }
 }
 
 fun main(args: Array<String>) {
-    var myDog = Dog("Fido", 70, "Mixed")
-    println(myDog.name)
-    myDog.weight = 75
+  var myDog = Dog("Fido", 70, "Mixed")
+  println(myDog.name)
+  myDog.weight = 75
 
-    val dogs = arrayOf(Dog("Fido", 70, "Mixed"), Dog("Ripper", 10, "Poodle"))
-    for (dog in dogs)
-        dog.bark()
+  val dogs = arrayOf(
+    Dog("Fido", 70, "Mixed"),
+    Dog("Ripper", 10, "Poodle")
+  )
+  for (dog in dogs)
+    dog.bark()
 }
