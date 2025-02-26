85193dba85a364c6446ec8c47ab717b34458b16a
nero <phuongsover2@gmail.com>
2025-02-26 23:23:44 +0700

    refactor: Use override val for Hippo image property
    The `image` property in the `Animal` class is now defined as `open val`
    instead of `var`. This allows subclasses like `Hippo` to override
    the property directly with a `val` instead of initializing it in an
    `init` block. This simplifies the code and improves readability.

--- head-first-kotlin/chapter5-inheritance/src/Animal.kt
+++ head-first-kotlin/chapter5-inheritance/src/Animal.kt
@@ -1,5 +1,5 @@
 open class Animal {
-  var image = ""
+  open val image = ""
   open val food = ""
   open val habitat = ""
   var hunger= 10
@@ -22,9 +22,7 @@ open class Animal {
 }
 
 class Hippo : Animal() {
-  init {
-    image = "hippo.jpg"
-  }
+  override val image = "hippo.jpg"
   override val food = "grass"
   override val habitat = "water"
 }
