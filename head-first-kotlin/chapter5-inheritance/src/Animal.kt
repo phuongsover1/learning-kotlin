8b096d7b10adaddfe550700f7715d50191262928
nero <phuongsover2@gmail.com>
2025-02-26 23:44:00 +0700

    feat: Implement makeNoise and eat functions for Hippo class
    This commit implements the `makeNoise` and `eat` functions for the `Hippo` class.
    - `makeNoise` prints "Grunt! Grunt!" to the console.
    - `eat` prints "The Hippo is eating grass" to the console, using the Hippo's `food` property.

--- head-first-kotlin/chapter5-inheritance/src/Animal.kt
+++ head-first-kotlin/chapter5-inheritance/src/Animal.kt
@@ -25,6 +25,14 @@ class Hippo : Animal() {
   override val image = "hippo.jpg"
   override val food = "grass"
   override val habitat = "water"
+
+  override fun makeNoise() {
+    println("Grunt! Grunt!")
+  }
+
+  override fun eat() {
+    println("The Hippo is eating $food")
+  }
 }
 fun main(args: Array<String>) {
 
