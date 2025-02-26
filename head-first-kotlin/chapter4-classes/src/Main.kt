fa0561520af8a553bd1a7fb602a8b6e68aedae91
nero <phuongsover2@gmail.com>
2025-02-26 20:10:39 +0700

    feat: Refactor Dog class with initializer and computed property
    This commit refactors the `Dog` class to:
    
    - Add an initializer block to print a message when a `Dog` object is created.
    - Introduce a computed property `weightInKgs` to calculate weight in kilograms.
    - Convert the breed to uppercase.
    - Add a default activity array.
    - Modified the constructor to directly use the parameter `breed_param`

--- head-first-kotlin/chapter4-classes/src/Main.kt
+++ head-first-kotlin/chapter4-classes/src/Main.kt
@@ -1,18 +1,26 @@
-//class Dog (val name: String, var weight: Int, val breed: String) {
-//    fun bark() {
-//        println(if (weight < 20) "Yip!" else "Woof!")
-//    }
-//}
+class Dog (val name: String, var weight: Int, breed_param: String) {
+  var activities = arrayOf("Walks")
+  val breed = breed_param.toUpperCase()
+  val weightInKgs: Double
+    get() = weight / 2.2
 
-class Dog(name_param: String, weight_param: Int, breed_param: String) {
-  val name = name_param
-  var weight = weight_param
-  val breed = breed_param
-
-  fun bark() {
-    println(if (weight < 20) "Yip!" else "Woof!")
+  init {
+    println("Dog $name has been created with $breed")
   }
+    fun bark() {
+        println(if (weight < 20) "Yip!" else "Woof!")
+    }
 }
+
+//class Dog(name_param: String, weight_param: Int, breed_param: String) {
+//  val name = name_param
+//  var weight = weight_param
+//  val breed = breed_param
+//
+//  fun bark() {
+//    println(if (weight < 20) "Yip!" else "Woof!")
+//  }
+//}
 
 fun main(args: Array<String>) {
   var myDog = Dog("Fido", 70, "Mixed")
